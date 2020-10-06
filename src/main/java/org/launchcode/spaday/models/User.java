12f7ab703f9806f9d1f856d5a0762f23d08fda22
Aaron <aaron.birkett@yahoo.com>
2020-10-05 20:29:49 -0500

    first commit oct 5

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,25 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Email not valid. Try again")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+//    @NotBlank(message = "Verify is required")
+//    @Size(min = 6, message = "Verify must be at least 6 characters")
+//    private String verify;
 
     public User() {
 
@@ -14,6 +30,7 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+        //this.verify = verify;
     }
 
     public String getUsername() {
@@ -39,4 +56,12 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+//    public String getVerify() {
+//        return verify;
+//    }
+//
+//    public void setVerify(String verify) {
+//        this.verify = verify;
+//    }
 }
